0686c1963f04e44e3dcc1b9e759fe5352fb65403
Karine Vitória <karinevitoria0073@gmail.com>
2023-10-19 19:59:25 -0300

    criando o navbar

--- app/testComponent/page.tsx
+++ app/testComponent/page.tsx
@@ -1,11 +1,14 @@
 import HomePageProduct from "@/components/HomePageProduct";
+import Navbar from "@/components/Navbar";
 import ProductCarousel from "@/components/ProductCarousel";
 
 export default async function TestComponent() {
     return (
+        <div>
         <ProductCarousel 
             title="Mais vendidos" 
             ProductComponent={HomePageProduct} 
         />
+        </div>
     )
 }
