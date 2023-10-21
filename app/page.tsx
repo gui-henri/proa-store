989aba2d9be08d8c00825a63c0f48fc1fa1c6f4c
adrianofsoaresti <guihenrique.dev@gmail.com>
2023-10-21 15:54:22 -0300

    added new dependencys

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
 import H1 from "@/components/Typography/H1";
-import H2 from "@/components/Typography/H2";
-import H3 from "@/components/Typography/H3";
 
 export default function Home() {
   return (
-    <>
-      <H1>Criar uma votação</H1>
-      <H2>Editar imagem do banner</H2>
-      <H3>Envie aqui o seu arquivo</H3>
-    </>
+    <H1>Hellow UwUrld</H1>
   )
 }
